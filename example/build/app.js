2734e57c62e9595952b3ee1503d7ec7221165979
papgueye <ggueye294@gmail.com>
2024-03-25 19:11:19 +0000

    add: organize in component

--- example/build/app.js
+++ example/build/app.js
@@ -3,143 +3,94 @@ class Activepage extends HTMLElement {
     constructor() {
         super();
         //this.attachShadow({ mode: 'open' });
-        
+        function Getdata(data) {
+      return data;
+    }
     }
     connectedCallback(){
         this.render()
     }
     render(){
         let content= `
-        <ul class="list-group">
-    <li
-        class="list-group-item d-flex justify-content-between align-items-center active"
-    >
-        Active list item
-        <span class="badge bg-secondary badge-pill">pill1</span>
-    </li>
-    <li
-        class="list-group-item d-flex justify-content-between align-items-center"
-    >
-        List item
-        <span class="badge bg-secondary badge-pill">pill2</span>
-    </li>
-    <li
-        class="list-group-item d-flex justify-content-between align-items-center disabled"
-    >
-        Disabled item
-        <span class="badge bg-secondary badge-pill">pill3</span>
-    </li>
-</ul>
+        <section class="todoapp" id="root">
+    <header class="header" data-testid="header">
+      <h1>todos</h1>
+      <div class="input-container">
+        <input
+          class="new-todo"
+          id="todo-input"
+          type="text"
+          data-testid="text-input"
+          placeholder="What needs to be done?"
+          value=""
+        />
+      </div>
+    </header>
+    <main class="main" data-testid="main">
+      <div class="toggle-all-container">
+        <input
+          class="toggle-all"
+          type="checkbox"
+          data-testid="toggle-all"
+        /><label class="toggle-all-label" for="toggle-all"
+          >Toggle All Input</label
+        >
+      </div>
+      <ul class="todo-list" data-testid="todo-list">
+          <hub-item>  <hub-item/>
+      </ul>
+    </main>
+    <hub-footer></hub-footer>
+  </section>
                 `;
         this.innerHTML=content
     }
 }
 customElements.define('hub-activepage', Activepage);
     
-class CompletedAllslaslapage extends HTMLElement {
-    constructor() {
-        super();
-        //this.attachShadow({ mode: 'open' });
-        
-    }
-    connectedCallback(){
-        this.render()
-    }
-    render(){
-        let content= `
-        <div class="btn-group-vertical" role="group" aria-label="">
-    <button type="button" class="btn btn-secondary">First One</button>
-    <button type="button" class="btn btn-secondary">Second One</button>
-    <div class="btn-group" role="group">
-        <button
-            id="dropdownId"
-            type="button"
-            class="btn btn-secondary dropdown-toggle"
-            data-bs-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-        >
-            More
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownId">
-            <a class="dropdown-item" href="#/">First Dropdown</a>
-            <a class="dropdown-item" href="#">Second Dropdown</a>
-        </div>
-    </div>
-</div>
-                `;
-        this.innerHTML=content
-    }
-}
-customElements.define('hub-completedallslaslapage', CompletedAllslaslapage);
-    
-class CompletedOnepage extends HTMLElement {
-    constructor() {
-        super();
-        //this.attachShadow({ mode: 'open' });
-        
-    }
-    connectedCallback(){
-        this.render()
-    }
-    render(){
-        let content= `
-        <div class="btn-group-vertical" role="group" aria-label="">
-    <button type="button" class="btn btn-secondary">First One</button>
-    <button type="button" class="btn btn-secondary">Second One</button>
-    <div class="btn-group" role="group">
-        <button
-            id="dropdownId"
-            type="button"
-            class="btn btn-secondary dropdown-toggle"
-            data-bs-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="false"
-        >
-            More
-            sd
-        </button>
-        <div class="dropdown-menu" aria-labelledby="dropdownId">
-            <a class="dropdown-item" href="#/">First Dropdown</a>
-            <a class="dropdown-item" href="#/completed">Second Dropdown</a>
-        </div>
-    </div>
-</div>
-                `;
-        this.innerHTML=content
-    }
-}
-customElements.define('hub-completedonepage', CompletedOnepage);
-    
 class Completedpage extends HTMLElement {
     constructor() {
         super();
         //this.attachShadow({ mode: 'open' });
-        
+        function Getdata(data) {
+      return data;
+    }
     }
     connectedCallback(){
         this.render()
     }
     render(){
         let content= `
-        <div class="dropdown open">
-    <button
-        class="btn btn-secondary dropdown-toggle"
-        type="button"
-        id="triggerId"
-        data-bs-toggle="dropdown"
-        aria-haspopup="true"
-        aria-expanded="false"
-    >
-        Dropdown Button
-    </button>
-    <div class="dropdown-menu" aria-labelledby="triggerId">
-        <button class="dropdown-item" href="#">Action</button>
-        <button class="dropdown-item disabled" href="#">
-            Disabled action
-        </button>
-    </div>
-</div>
+        <section class="todoapp" id="root">
+    <header class="header" data-testid="header">
+      <h1>todos</h1>
+      <div class="input-container">
+        <input
+          class="new-todo"
+          id="todo-input"
+          type="text"
+          data-testid="text-input"
+          placeholder="What needs to be done?"
+          value=""
+        />
+      </div>
+    </header>
+    <main class="main" data-testid="main">
+      <div class="toggle-all-container">
+        <input
+          class="toggle-all"
+          type="checkbox"
+          data-testid="toggle-all"
+        /><label class="toggle-all-label" for="toggle-all"
+          >Toggle All Input</label
+        >
+      </div>
+      <ul class="todo-list" data-testid="todo-list">
+        <hub-item>  <hub-item/>
+      </ul>
+    </main>
+    <hub-footer></hub-footer>
+  </section>
                 `;
         this.innerHTML=content
     }
@@ -170,7 +121,7 @@ class page extends HTMLElement {
         data-testid="text-input"
         placeholder="What needs to be done?"
         value=""
-      /><label class="visually-hidden" for="todo-input">New Todo Input</label>
+      />
     </div>
   </header>
   <main class="main" data-testid="main">
@@ -184,10 +135,29 @@ class page extends HTMLElement {
       >
     </div>
     <ul class="todo-list" data-testid="todo-list">
-        <hub-todo>  <hub-todo/>
+        <hub-item>  <hub-item/>
     </ul>
   </main>
-  <footer class="footer" data-testid="footer">
+  <hub-footer></hub-footer>
+</section>
+                `;
+        this.innerHTML=content
+    }
+}
+customElements.define('hub-page', page);
+    
+class footer extends HTMLElement {
+    constructor() {
+        super();
+        //this.attachShadow({ mode: 'open' });
+        
+    }
+    connectedCallback(){
+        this.render()
+    }
+    render(){
+        let content= `
+        <footer class="footer" data-testid="footer">
     <span class="todo-count">1 item left!</span>
     <ul class="filters" data-testid="footer-navigation">
       <li><a class="selected" href="#/">All</a></li>
@@ -196,14 +166,13 @@ class page extends HTMLElement {
     </ul>
     <button class="clear-completed" disabled="">Clear completed</button>
   </footer>
-</section>
                 `;
         this.innerHTML=content
     }
 }
-customElements.define('hub-page', page);
+customElements.define('hub-footer', footer);
     
-class todo extends HTMLElement {
+class item extends HTMLElement {
     constructor() {
         super();
         //this.attachShadow({ mode: 'open' });
@@ -215,21 +184,17 @@ class todo extends HTMLElement {
     render(){
         let content= `
         <li class="" data-testid="todo-item">
-        <div class="view">
-          <input
-            class="toggle"
-            type="checkbox"
-            data-testid="todo-item-toggle"
-          /><label data-testid="todo-item-label">label alpapie</label
-          ><button class="destroy" data-testid="todo-item-button"></button>
-        </div>
-      </li>
-      jjzjx
+  <div class="view">
+    <input class="toggle" type="checkbox" data-testid="todo-item-toggle" />
+    <label data-testid="todo-item-label">task 1</label>
+    <button class="destroy" data-testid="todo-item-button"></button>
+  </div>
+</li>
                 `;
         this.innerHTML=content
     }
 }
-customElements.define('hub-todo', todo);
+customElements.define('hub-item', item);
     
 
 export default class Router extends HTMLElement {
@@ -243,18 +208,6 @@ export default class Router extends HTMLElement {
                     name: 'hub-activepage',
                     hash: '/active',
                     regExp: new RegExp(/^#\/active$/)
-                }
-                ,
-                {
-                    name: 'hub-completedallslaslapage',
-                    hash: '/completed/allslasla',
-                    regExp: new RegExp(/^#\/completed\/allslasla$/)
-                }
-                ,
-                {
-                    name: 'hub-completedonepage',
-                    hash: '/completed/one',
-                    regExp: new RegExp(/^#\/completed\/one$/)
                 }
                 ,
                 {
